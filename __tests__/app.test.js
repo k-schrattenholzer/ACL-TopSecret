204a22137b095ab33cd3c4aea6ce8d19803a7bb7
Katie Schrattenholzer <ktschr11@gmail.com>
2022-02-07 21:10:26 -0800

    set up user model, and controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,15 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+
+const testUser = {
+  firstName: 'Dinky',
+  lastName: 'Kong',
+  email: 'dinky@kong.com',
+  password: '12345',
+};
+
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -12,7 +21,15 @@ describe('backend routes', () => {
     pool.end();
   });
 
-  it('placeholder', async () => {
-    expect(1).toEqual(1);
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(testUser);
+    const { firstName, lastName, email } = testUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
