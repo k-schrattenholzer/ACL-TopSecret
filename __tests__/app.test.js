f2c25e838348d0a3d6c7f1b702c344a258a20506
Katie Schrattenholzer <ktschr11@gmail.com>
2022-02-07 21:36:59 -0800

    protected the /me route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,8 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
+const pool = require('../lib/utils/pool')
+const setup = require('../data/setup')
+const request = require('supertest')
+const app = require('../lib/app')
+const UserService = require('../lib/services/UserService')
 
 
 const testUser = {
@@ -9,27 +10,55 @@ const testUser = {
   lastName: 'Kong',
   email: 'dinky@kong.com',
   password: '12345',
+}
+
+const registerAndSignIn = async (userProps = {}) => {
+  const password = userProps.password ?? testUser.password
+
+  const agent = request.agent(app)
+
+  const user = await UserService.create({ ...testUser, ...userProps })
+
+  const { email } = user
+
+  await agent.post('/api/v1/users/sessions').send({ email, password })
+
+  return [agent, user]
 };
 
 
 describe('backend routes', () => {
   beforeEach(() => {
-    return setup(pool);
-  });
+    return setup(pool)
+  })
 
   afterAll(() => {
-    pool.end();
-  });
+    pool.end()
+  })
 
   it('creates a new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(testUser);
-    const { firstName, lastName, email } = testUser;
+    const res = await request(app).post('/api/v1/users').send(testUser)
+    const { firstName, lastName, email } = testUser
 
     expect(res.body).toEqual({
       id: expect.any(String),
       firstName,
       lastName,
       email,
-    });
-  });
-});
+    })
+  })
+
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndSignIn()
+
+    const me = await agent.get('/api/v1/users/me')
+
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    })
+  })
+
+  /////////////////// new tests above ///////////////////
+})
